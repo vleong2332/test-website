16d409088285d2b20c85f98d98adbbecf8f2d5c8
Vicky Leong <vleong2332@gmail.com>
2017-04-22 15:51:06 -0400

    Add example to pull out item from entries

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,37 @@ var contentful = require('contentful');
 var mustache = require('gulp-mustache');
 var rimraf = require('rimraf');
 
+var client = contentful.createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.API_TOKEN
+});
+
 
 gulp.task('clean', function() {
   rimraf.sync('public');
 });
 
+var ENTRIES = {
+  morningSermons: '1Qy66Ai4pqiMK2gYO2MCqu'
+};
+
 gulp.task('build', function() {
-  return gulp.src('src/**/*')
-    .pipe(mustache({
-      sermons: [{
-        title: "Hello, there"
-      }]
-    }))
-    .pipe(gulp.dest('public'));
+  return client.getEntries({ limit: 1000 }).then(function(entries) {
+    var items = entries.items;
+    var morningSermons = items.find(function(item) {
+      return item.sys.id == ENTRIES['morningSermons'];
+    });
+    var morningSermonsList = morningSermons.fields.sermons.map(function(sermon) {
+      return {
+        title: sermon.fields.title
+      };
+    });
+    return gulp.src('src/**/*')
+      .pipe(mustache({
+        sermons: morningSermonsList
+      }))
+      .pipe(gulp.dest('public'));
+  });
 });
 
 gulp.task('default', ['clean', 'build']);
