d15dd43dd281951beae185d72659480812305d69
Vicky Leong <vleong2332@gmail.com>
2017-04-23 19:43:36 -0400

    Switch example to meetups

--- gulpfile.js
+++ gulpfile.js
@@ -15,27 +15,46 @@ gulp.task('clean', function() {
   rimraf.sync('public');
 });
 
-var ENTRIES = {
-  morningSermons: '1Qy66Ai4pqiMK2gYO2MCqu'
-};
-
 gulp.task('build', function() {
   return client.getEntries({ limit: 1000 }).then(function(entries) {
-    var items = entries.items;
-    var morningSermons = items.find(function(item) {
-      return item.sys.id == ENTRIES['morningSermons'];
-    });
-    var morningSermonsList = morningSermons.fields.sermons.map(function(sermon) {
-      return {
-        title: sermon.fields.title
-      };
-    });
-    return gulp.src('src/**/*')
+    return gulp.src('src/**/*.html')
       .pipe(mustache({
-        sermons: morningSermonsList
+        meetups: getMeetups(entries)
       }))
       .pipe(gulp.dest('public'));
   });
 });
 
+gulp.task('show-entries', function() {
+  return client.getEntries({ limit: 1000 }).then(function(entries) {
+    console.log(JSON.stringify(entries, null, 2));
+  });
+});
+
 gulp.task('default', ['clean', 'build']);
+
+
+function getMeetups(entries) {
+  return entries.items.filter(function(item) {
+    return item.sys.contentType.sys.id == 'meetup';
+  })
+  .map(function(meetup) {
+    return {
+      id: meetup.sys.id,
+      name: meetup.fields.name,
+      city: meetup.fields.city,
+      organizers: meetup.fields.organizers.map(function(person) {
+        return {
+          name: person.fields.name,
+          email: person.fields.email
+        };
+      }),
+      members: meetup.fields.members.map(function(person) {
+        return {
+          name: person.fields.name,
+          email: person.fields.email
+        };
+      })
+    }
+  });
+}
